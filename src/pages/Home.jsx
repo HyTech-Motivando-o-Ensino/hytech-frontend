1b4d517231118dd46647bdff389453c93d77c97e
FabioKrakauer <comvocefabio@gmail.com>
2021-11-20 10:15:55 -0300

    improve backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,18 @@ import { Container } from 'reactstrap';
 import Message from '../components/Message/Message';
 
 
-import {ReactComponent as SendIcon} from '../assets/Buttons/send.svg';
-import {ReactComponent as VerticalDot} from '../assets/Buttons/vertical-dot.svg';
+import { ReactComponent as SendIcon } from '../assets/Buttons/send.svg';
+import { ReactComponent as VerticalDot } from '../assets/Buttons/vertical-dot.svg';
 
 import { useContext, useEffect, useRef, useState } from 'react';
 import { HomeContext } from '../context/home';
 
 import './Home.css';
 import hello from '../hooks/hello';
+import logo from '../assets/avatar.png';
 
 function Home(props) {
-    const {components, addComponent} = useContext(HomeContext);
+    const { components, addComponent } = useContext(HomeContext);
     const [rendered, setRendered] = useState(false);
     const messageEl = useRef(null);
 
@@ -20,27 +21,29 @@ function Home(props) {
         messageEl.current.scrollIntoView({ behavior: "smooth" })
     };
     const helloComp = hello();
-    
+
     useEffect(() => {
-        if(rendered == false) {
+        if (rendered == false) {
             helloComp.getComponent(addComponent);
             setRendered(true);
         }
     })
     useEffect(() => {
         if (messageEl) {
-          messageEl.current.addEventListener('DOMNodeInserted', event => {
-            const { currentTarget: target } = event;
-            target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
-          });
+            messageEl.current.addEventListener('DOMNodeInserted', event => {
+                const { currentTarget: target } = event;
+                target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
+            });
         }
-      }, [])
+    }, [])
 
     return (
-        <Container>
+        <Container className="col-3">
             <div className="header">
+                <div className="bg-avatar mr-2">
+                    <img src={logo} alt="logo" className="avatar" />
+                </div>
                 <h1>Cesinha</h1>
-                <VerticalDot />
             </div>
             <div className="conversation" ref={messageEl}>
                 {components.map((data) => (
@@ -49,9 +52,8 @@ function Home(props) {
                     </Message>
                 ))}
             </div>
-            <Container>
-                {<input type="text" id="message" className="message-input shadow col-11" disabled /> }
-                <button className="btn bg-orange" ><SendIcon fill='#FD813B' /></button>
+            <Container className="bg-input">
+                {<input type="text" id="message" placeholder="Mensagem" className="message-input shadow col-12 mb-3" />}
             </Container>
         </Container>
     )
